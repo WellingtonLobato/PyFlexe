d7e6060d923569a8a97838d58a1ff12f96149cf6
Wellington Viana Lobato Junior <wellington.lobato.junior@gmail.com>
2023-07-03 16:59:30 +0200

    Update server_flexe.py

--- server_flexe.py
+++ server_flexe.py
@@ -9,11 +9,16 @@ from core.application.tf.StoreApp import StoreApp
 import sys
 import os
 
-def serve(ip_server, n_clients, dataset, non_iid):
+def serve(ip_server, n_clients, dataset, n_classes, model_name, non_iid):
     print("+-------------------------------+")
     print("|       GRPC Server Start!      |")
     print("+-------------------------------+")
     print("Server IP address: " + ip_server)
+    print("Number of Clients: " + str(n_clients))
+    print("Dataset: " + dataset)
+    print("Number of Classes: " + str(n_classes))
+    print("Type of Model: " + model_name)
+    print("Non-IID? " + str(non_iid))
     print()
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10),
     options = [
@@ -23,9 +28,9 @@ def serve(ip_server, n_clients, dataset, non_iid):
         ('grpc.keepalive_timeout_ms', 7200000),
         ('grpc.grpclb_call_timeout_ms', 7200000)
     ])
-    flexe_pb2_grpc.add_FlexeServicer_to_server(StoreApp(n_clients, dataset, non_iid), server)
+    flexe_pb2_grpc.add_FlexeServicer_to_server(StoreApp(n_clients, dataset, n_classes, model_name, non_iid), server)
     
-    server.add_insecure_port(ip_server+':5000')
+    server.add_insecure_port(ip_server)
     server.start()
     server.wait_for_termination()
 
@@ -33,12 +38,11 @@ if __name__ == '__main__':
     parser = OptionParser()
     
     parser.add_option("-c", "--clients",     		dest="n_clients",          default=1,        help="Number of clients in the simulation",    metavar="INT")
-    parser.add_option("-i", "--ip",    		        dest="ip_address",          default='127.0.0.1',  help="Strategy of the federated learning",  metavar="STR")
+    parser.add_option("-i", "--ip",    		        dest="ip_address",         default='127.0.0.1:5000',  help="Strategy of the federated learning",  metavar="STR")
     parser.add_option("-s", "--strategy",    		dest="strategy_name",      default='FedSGD',  help="Strategy of the federated learning",     metavar="STR")
-    parser.add_option("-a", "--aggregation_method", dest="aggregation_method", default='None',    help="Algorithm used for selecting clients",   metavar="STR")
-    parser.add_option("-m", "--model",       		dest="model_name",         default='DNN',     help="Model used for trainning",               metavar="STR")
+    parser.add_option("-a", "--aggregation_method", 	dest="aggregation_method", default='None',    help="Algorithm used for selecting clients",   metavar="STR")
+    parser.add_option("-m", "--model",       		dest="model_name",         default='GENERIC', help="Model used for trainning",               metavar="STR")
     parser.add_option("-d", "--dataset",     		dest="dataset",            default='MNIST',   help="Dataset used for trainning",             metavar="STR")
-    parser.add_option("",   "--poc",         		dest="poc",                default=0,         help="Percentage clients to be selected",      metavar="FLOAT")
     parser.add_option("",   "--decay",       		dest="decay",              default=0,         help="Decay factor for FedLTA",                metavar="FLOAT")
     parser.add_option("",   "--non-iid",     		dest="non_iid",            default=False,     help="Non IID distribution",                   metavar="BOOLEAN")
     parser.add_option("-y", "--classes",     		dest="n_classes",          default=10,        help="Number of classes",                      metavar="INT")
@@ -46,4 +50,4 @@ if __name__ == '__main__':
     
     (opt, args) = parser.parse_args()
     logging.basicConfig()
-    serve(opt.ip_address, opt.n_clients, opt.dataset, opt.non_iid)
+    serve(opt.ip_address, opt.n_clients, opt.dataset, opt.n_classes, opt.model_name, opt.non_iid)
